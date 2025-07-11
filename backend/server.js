9bef5cf36aaa9ceeaadb8e93f069fa632434578f
Darshan <darsuprajapati262@gmail.com>
2025-07-11 20:47:54 +0530

    change server js

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,42 @@
 import express from 'express'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import cors from 'cors'
-import swaggerUi from "swagger-ui-express"
+import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-import connectDB from './config/db.js';
-import MainRouter from "./routes/main.routes.js"
-import { errorHandler } from './middleware/error.middleware.js';
+import connectDB from './config/db.js'
+import MainRouter from './routes/main.routes.js'
+import { errorHandler } from './middleware/error.middleware.js'
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config()
 connectDB()
 
-// Create an Express application
 const app = express()
 
-// global middleware
 app.use(express.json())
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
-}))
+    credentials: true,
+  })
+)
 
-// Error handling middleware
+const swaggerDoc = YAML.load(path.join(__dirname, 'openapi.yaml'))
+
+app.use('/api', MainRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
 app.use(errorHandler)
 
-// const swaggerDoc = YAML.load('./openapi.yaml')
-
-// api route end Point
-app.use("/api", MainRouter)
-// app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDoc))
-
-// Start the server
 // const PORT = process.env.PORT || 8080
-
 // app.listen(PORT, () => {
 //     console.log(`Server running on port http://localhost:${PORT}`)
 // })
 
 export default app
+
